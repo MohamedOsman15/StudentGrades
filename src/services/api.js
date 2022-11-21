09638e5e8704d95d8ed76621de1b6d10dfa874f8
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-11-21 10:31:39 -0500

    Added services folder with auth functions

---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import { DB_URL } from '../global'
+
+const Client = axios.create({ baseURL: DB_URL })
+
+Client.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      config.headers['authorization'] = `Bearer ${token}`
+    }
+  },
+  (error) => Promise.reject(error)
+)
+
+export default Client
